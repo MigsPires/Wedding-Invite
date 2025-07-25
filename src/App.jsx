bf3d35890d49d9c60c7a2d810e1d78d429b2fdb5
MigsPires <39861051+MigsPires@users.noreply.github.com>
2025-07-25 15:38:47 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import CountdownPage from './CountdownPage';
+import InvitePage from './InvitePage';
+
+const App = () => {
+  const [showInvite, setShowInvite] = useState(false);
+  return showInvite ? <InvitePage /> : <CountdownPage onEnter={() => setShowInvite(true)} />;
+};
+
+export default App;
