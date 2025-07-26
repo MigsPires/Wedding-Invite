ff13f7e40d750fe127300f6670199d97ad4907f7
Rui Pires <rui.pires@arquiconsult.com>
2025-07-26 15:19:05 +0100

    cahnges

--- src/CountdownPage.jsx
+++ src/CountdownPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const weddingDate = new Date('2026-05-22T12:30:00');
 
+
 const CountdownPage = ({ onEnter }) => {
   const [time, setTime] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
@@ -22,17 +23,29 @@ const CountdownPage = ({ onEnter }) => {
 
   return (
     <div className="countdown-page" style={{ backgroundImage: "url('/fundo.jpeg')" }}>
-      <div className="overlay"style={{ backgroundImage: "url('/br.jpeg')" }} >
+       <div className ="b " >
+  <div className="overlay">
+  <img
+    src="/logo.png"
+    className="logo"
+    alt="Logo Beatriz & Rui"
+  />q
+  </div>
+      <div className="overlay">
         <div className="countdown-container">
           <div className="time-box"><span>{time.days}</span><small>Dias</small></div>
           <div className="time-box"><span>{time.hours}</span><small>Horas</small></div>
           <div className="time-box"><span>{time.minutes}</span><small>Min</small></div>
           <div className="time-box"><span>{time.seconds}</span><small>Seg</small></div>
         </div>
+    
         <button className="enter-btn" onClick={onEnter}>💌 Ver Convite</button>
+            </div>
       </div>
     </div>
   );
 };
+
+
 
 export default CountdownPage;
